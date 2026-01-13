7a3d1fa144ac7b7da9f6010280eec75a73756995
calebeandrade93 <andrade.calebe@outlook.com>
2026-01-13 18:00:27 -0300

    configuracao inicial do projeto, iniciado container da aplicacao e conexao com banco de dados

---
+++ src/test/java/br/com/vr/mini_autorizador/MiniAutorizadorApplicationTests.java
@@ -0,0 +1,10 @@
+package br.com.vr.mini_autorizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAutorizadorApplicationTests {
+
+
+}
