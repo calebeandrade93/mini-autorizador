acfa7f3cf3ba83d6fd07bf03353b75066a6e8cfa
calebeandrade93 <andrade.calebe@outlook.com>
2026-01-14 11:40:18 -0300

    Endpoint transacoes + ajustes no código

--- src/main/java/br/com/vr/mini_autorizador/exceptions/CartaoNaoEncontrado.java
+++
@@ -1,7 +0,0 @@
-package br.com.vr.mini_autorizador.exceptions;
-
-public class CartaoNaoEncontrado extends RuntimeException {
-    public CartaoNaoEncontrado(String message) {
-        super(message);
-    }
-}
